848c4edc35ad86fd7205e2dd95bcdb77078f4735
lxhau <62015621+lxhau@users.noreply.github.com>
2021-01-23 23:12:22 +0700

    Update database

--- routes/createUser.js
+++ routes/createUser.js
@@ -3,14 +3,5 @@ const Router = express.Router();
 const mysql = require("../configs/database");
 const request = require('request');
 
-Router.post('/', function(req, res){
-    var post_data = req.body;
-    console.log(post_data);
-    mysql.query('INSERT INTO `nhantin`(`id`, `lastName`, `firstName`) VALUES (?)',[post_data.userId,post_data.lastName,post_data.firstName],(err, rows) => {
-        if(err){
-        }
-    })
-    res.status(200).end();
-})
 
 module.exports =Router;
--- webhooks.js
+++ webhooks.js
@@ -18,7 +18,7 @@ dotenv.config({
 });
 
 app.use("/getDiem",getDiem);
-app.use("/createUser", createUser);
+//app.use("/createUser", createUser);
 
 const server = app.listen(process.env.PORT || 5000, () => {
   console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
